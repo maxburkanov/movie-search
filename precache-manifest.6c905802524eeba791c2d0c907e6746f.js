b06234d30644631be3bfab1a1cba6f774d92075d
Max Burkanov <ostu.student@gmail.com>
2020-10-29 02:05:15 -0400

    Updates

---
+++ precache-manifest.6c905802524eeba791c2d0c907e6746f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85dd7b5760a64c21224f5bd5fbec35d6",
+    "url": "/maxburkanov/movie-search.git/index.html"
+  },
+  {
+    "revision": "f73b560adfd4667ec781",
+    "url": "/maxburkanov/movie-search.git/static/css/main.7fe2c67e.chunk.css"
+  },
+  {
+    "revision": "3079229cb4008b8e50f0",
+    "url": "/maxburkanov/movie-search.git/static/js/2.e3e2a3d6.chunk.js"
+  },
+  {
+    "revision": "c9b2d83af0adefd71314e4f625c6828c",
+    "url": "/maxburkanov/movie-search.git/static/js/2.e3e2a3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73b560adfd4667ec781",
+    "url": "/maxburkanov/movie-search.git/static/js/main.1348d3e2.chunk.js"
+  },
+  {
+    "revision": "4e4da7c8fd58ca53b3a2",
+    "url": "/maxburkanov/movie-search.git/static/js/runtime-main.dd8e2ad5.js"
+  }
+]);
